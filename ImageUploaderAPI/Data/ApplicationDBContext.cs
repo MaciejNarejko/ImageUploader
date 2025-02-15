a6bb6eab69b35169abec26305dad5dfb5d6bd94a
Maciej Narejko <maciejnarejko@gmail.com>
2025-02-15 15:12:44 +0100

    Initial commit: Project initialization, added configuration, DbContext; initialized React frontend (Vite + TypeScript)

---
+++ ImageUploaderAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ImageUploaderAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
